556c859074595793fb08530623dd5784385ba411
Prateek Gogia <gogia.prateek@hotmail.com>
2020-09-15 22:50:23 +0530

    basic next.js setup

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+const Page = () => (
+  <div>
+    <Head>
+      <title>Next.js + Tailwind Starter</title>
+      <link rel="shortcut icon" href="/favicon.png" />
+    </Head>
+    Welcome to Next.js starter
+  </div>
+);
+
+export default Page;
